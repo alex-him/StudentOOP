4a3b80429a488e181716d7257bbf82bcdc89ea0c
Alex Him <alex.him@nyu.edu>
2019-06-05 19:20:54 -0400

    Basics

---
+++ my_code/basics.h
@@ -0,0 +1,16 @@
+//
+// Created by Alexander Him on 2019-05-31.
+//
+
+#ifndef GITHUB_BASICS_H
+#define GITHUB_BASICS_H
+
+
+
+class basics {
+
+};
+
+
+
+#endif //GITHUB_BASICS_H
---
+++ my_code/test_basics.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alexander Him on 2019-05-31.
+//
+
+#include "test_basics.h"
