766f83ec928f47e597681bb0b3ead544708a1693
Alex Him <alex.him@nyu.edu>
2019-06-12 12:41:26 -0400

    weather lab

---
+++ my_code/complex.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <istream>
+#include <iomanip>
+#include "complex.h"
+
+using namespace std;
+
+bool operator== (const Complex& c1, const Complex& c2) {
+    return true;  // temp!
+}
+
+ostream& operator<< (ostream& os, const Complex& c) {
+    /*
+     * Outputting a `Complex` instance, while illustrating some of the
+     * capabilities of I/O streams: `setprecision` gives us a fixed
+     * number of decimal places, while `showpos` turns on the plus
+     * sign for positive numbers.
+     * */
+    return os;
+}
+
+
+/*
+ * Read a `Complex` number from an input stream.
+ * */
+istream& operator>> (istream& is, Complex& c) {
+    return is;
+}
+
+Complex::Complex(double real, double imag) {}
+
+Complex::operator bool() const {
+    return true;
+}
+
+Complex& Complex::operator++() {
+    return *this;
+}
+
+Complex Complex::operator++(int dummy) {
+    return *this;
+}
+
+double Complex::get_real() const {
+    return real;
+}
+
+double Complex::get_imag() const {
+    return imag;
+}
+
+Complex Complex::operator*(const int i) {
+    return *this;
+}
