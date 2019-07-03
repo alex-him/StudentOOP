4fe5223ab765ce79b674ec25a971050a5a4e9fb4
Alex Him <alexhim@10-20-155-120.dynapool.wireless.nyu.edu>
2019-07-03 00:03:13 -0400

    oops wrong file

--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -1,94 +1,65 @@
 #include <iostream>
 #include "pointers.h"
-#include <vector>
 
 using namespace std;
 
-/*
- * Increment n.
- * */
 void inc_by_value(int n) {
 	n++;
 }
 
 
-/*
- * Increment n.
- * */
 void inc_with_pointer(int* iptr) {
-	*iptr = ((*iptr)+1);
+	(*iptr)++;
 }
 
 
-/*
- * Increment n.
- * */
 void inc_with_reference(int& n) {
 	n++;
 }
 
 
-/*
- * Create an array of n things, each one with its val set to
- * its index in the Thing array.
- * */
-Thing** create_array_of_things(int n)  {
-	Thing** array_of_things = new Thing*[n];
-
-	for (int i = 0; i < n; i++){
-		array_of_things[i] = new Thing(i);
+Thing** create_array_of_things(int n) {
+	Thing** thing = new Thing* [n];
+	for (int i=0; i<n; i++){
+		thing[i]=new Thing(i);
 	}
-    return array_of_things;
+    return thing;
 }
 
-/*
- * Print all of the things created in the function above.
- * */
 void print_all_things(Thing** things, int n) {
-	for (int i = 0; i < n; i ++){
-		cout << things[i]->val << ' ';
+	for (int i=0; i<n; i++){
+		cout << things[i]->val << " ";
 	}
 	cout << endl;
 }
 
-/*
- * Double the value of each thing (use *=).
- * */
 void double_all_things(Thing** things, int n) {
-	for (int index = 0; index < n; index ++){
-		things[index]->val *= 2;
+	for (int i=0; i<n; i++){
+		things[i]->val*=2;
 	}
 }
 
-
-/*
- * Delete all of the things, and the Thing array, that you have created.
- * */
 void delete_all_things(Thing** things, int n) {
-	for (int index = 0; index < n; index ++){
-		delete things[index];
+	for (int i=0; i<n; i++){
+		delete things[i];
+		things[i] = nullptr;
 	}
-	delete things;
+	delete[] things;
+	things = nullptr;
 }
 
 
-/*
- * Assign student `s` the TA `ta`.
- * */
 void assignTA(Student* s, Student* ta) {
-	s->ta = ta;
+	s -> ta = ta;
 }
 
-
-/*
- * Print who has which TA.
- * */
 void printTAs(vector<Student*>& students) {
-	vector ref_vec = students;
-	for (int i = 0; i < ref_vec.size(); i++){
-		Student *student_ref = ref_vec[i];
-		string stu_name = student_ref->name;
-		cout << stu_name << endl ; //<< ta_name;
-} }
-
-
+	for (Student* student : students){
+		if (student -> ta == nullptr){
+			cout << student->name << " has no TA\n";
+		}
+		else {
+			cout << student->name << student->ta->name << endl;
+		}
+	}
+}
