7a7102eaca31f3d821f32b58a54b4b5d090e4a39
Alex Him <alexhim@10-20-155-120.dynapool.wireless.nyu.edu>
2019-07-03 00:09:13 -0400

    omg why isnt it working pls work

--- my_code/vector.cpp
+++ my_code/vector.cpp
@@ -1,47 +1,32 @@
-#include <iostream>
-#include "vector.h"
-
+# include <iostream>
+# include "vector.h"
+# include <algorithm>
 using namespace std;
 
 void print_vector(const MyVec& v) {
-    cout << "[";
-    for(int i = 0; i < v.size(); i++){
-        cout << v[i];
-        if(i != (v.size() - 1)){ cout <<  ",";}
-    }
-    cout << "]" << endl;
+    for (int i = 0; i < v.size(); i++) cout << i << " ";
+    cout << endl;
 }
 
+MyVec::MyVec() : sz(0) {
+    capacity = DEF_CAPACITY;
+    data = new int[DEF_CAPACITY];
+}
 
 MyVec::MyVec(int sz, int val) : sz{sz} {
-    data = new int[max(DEF_CAPACITY, 2 * sz)];
-    capacity = 2 * sz;
-    for(int i=0; i < sz; i++){
-        data[i] = val;
-    }
-}
-
-MyVec::Iterator MyVec::begin() const {
-    return MyVec::Iterator(data);
-}
-
-MyVec::Iterator MyVec::end() const {
-    return MyVec::Iterator(data+sz);
-}
-
-
-MyVec::MyVec() {
-    data = new int[DEF_CAPACITY];
-    sz = 0;
-    capacity = DEF_CAPACITY;
+    capacity = max(DEF_CAPACITY, sz * CAPACITY_MULT);
+    data = new int[capacity];
+    for (size_t i = 0; i < sz; i++) data[i] = val;
 }
 
 MyVec::MyVec(const MyVec& v2) {
     capacity = v2.capacity;
-    sz = v2.sz;
+    sz = v2.size();
+
     data = new int[capacity];
-    for(int i=0; i < sz; i++){
-        data[i] = v2[i];
+    // copy over existing data
+    for (int i = 0; i < size(); i++) {
+        data[i] = v2.data[i];
     }
 }
 
@@ -50,45 +35,38 @@ MyVec::~MyVec() {
 }
 
 MyVec& MyVec::operator=(const MyVec& v2) {
-    if(this == &v2){ return *this; }
-    delete[] data;
-    capacity = v2.capacity;
-    sz = v2.sz;
-    data = new int[capacity];
-    for(int i=0; i < sz; i++){
-        data[i] = v2[i];
+    if (&v2 != this) {
+        delete[] data;
+        capacity = v2.capacity;
+        sz = v2.size();
+        data = new int[capacity];
+        // copy over existing data
+        for (int i = 0; i < size(); i++) {
+            data[i] = v2.data[i];
+        }
     }
     return *this;
-}
-
-
-/*
- * == is true when every element of the vectors are the same in
- * the same order. (Thus they must be the same size.)
- * */
-bool operator==(MyVec& v1, MyVec& v2) {
-    if(v1.size() != v2.size()){ return false; }
-    for(int i =0; i < v1.size(); i++){
-        if(v1[i] != v2[i]){ return false; }
-    }
-    return true;
 }
 
 /*
  * Puts an element at the back of a vector.
  * */
 void MyVec::push_back(int val) {
-    if(sz == capacity) {
+    if (sz == capacity) {
+        cout << "Increasing capacity\n";
+        // get new array of capacity * 2
         capacity *= 2;
         int* new_data = new int[capacity];
-        for(int i =0; i < sz; i++){
+        // copy over existing data
+        for (int i = 0; i < size(); i++) {
             new_data[i] = data[i];
         }
+        // delete old array
         delete[] data;
+        // set pointer to new array
         data = new_data;
     }
-    data[sz] = val;
-    sz ++;
+    data[sz++] = val;  // data[sz] = val; sz += 1;
 }
 
 /*
